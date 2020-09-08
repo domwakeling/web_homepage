8c9ea8081acb545cbe4eb5493ff844c3db8ba214
Dom Wakeling <domwakeling@gmail.com>
2020-09-08 21:58:24 +0100

    Add beautify HTML config + "clean" script

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,21 @@
-module.exports = {
-    dir: {
-        input: "src",
-        output: "_site"
+const beautify_html = require("js-beautify").html;
+
+module.exports = function (eleventyConfig) {
+
+    // beautify HTML files when output => ensures indentation is correct
+    eleventyConfig.addTransform("beautifyHTML", function (content, outputPath) {
+        if (outputPath.endsWith(".html")) {
+            return beautify_html(content);
+        }
+
+        return content;
+    });
+
+    // because we're making a function we need to return the "normal" exports object
+    return {
+        dir: {
+            input: "src",
+            output: "_site"
+        }
     }
 };
