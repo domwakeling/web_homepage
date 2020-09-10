b598bcf01357d548230df70650920fa03bf4080d
Dom Wakeling <domwakeling@gmail.com>
2020-09-10 18:45:15 +0100

    Added RealTimeWWII tweets

--- .eleventy.js
+++ .eleventy.js
@@ -21,6 +21,26 @@ module.exports = function (eleventyConfig) {
         return JSON.stringify(value);
     });
 
+    eleventyConfig.addNunjucksFilter("tweetbodytext", function (item) {
+        // receives a tweet item
+        let t = item.full_text;
+        let u = item.entities.urls;
+        const htmlRegex = /^(.*) ((([A-Za-z]{5,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/
+        if (!u || u.length == 0) {
+            m = t.match(htmlRegex)
+            return m ? m[1] : t
+        }
+        for (url of u) {
+            t = t.replace(url.url, `<a href="${url.expanded_url}">${url.display_url}</a>`)
+        }
+        return t;
+    });
+
+    eleventyConfig.addNunjucksFilter("datefromtweet", function(item) {
+        let t = item.created_at.match(/^(\w{3}) (\w{3}) (\d*) (\d*:\d*)/)
+        return `${t[1]} ${parseInt(t[3])} ${t[2]} - ${t[4]}`
+    })
+
     eleventyConfig.addFilter("cssmin", function (code) {
         return new CleanCSS({}).minify(code).styles;
     });
