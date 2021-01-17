a6667ea26ffe1855adf1080441df12fa5894c390
Dom Wakeling <domwakeling@gmail.com>
2021-01-17 22:39:35 +0000

    Added currency exchange rates (again)

---
+++ src/_data/currency.js
@@ -0,0 +1,48 @@
+const axios = require('axios');
+require('dotenv').config()
+
+module.exports = async function () {
+
+    // in development, send back a static object
+    if (process.env.LOCAL_DEVELOPMENT == 'DEVELOPMENT') return [{
+        flag: 'https://www.countryflags.io/gb/flat/64.png',
+        symbol: '&pound;',
+        rate: '1.00'
+    },
+    {
+        flag: 'https://www.countryflags.io/eu/flat/64.png',
+        symbol: '&euro;',
+        rate: '1.12'
+    },
+    {
+        flag: 'https://www.countryflags.io/us/flat/64.png',
+        symbol: '$',
+        rate: '1.36'
+    }];
+
+    // get the current data
+    let ratedata = await axios
+        .get(`https://api.exchangeratesapi.io/latest?base=GBP&symbols=EUR,USD`)
+        .then(res => res.data.rates)
+        .catch((err) => {
+            console.error(err);
+            return {};
+        });
+
+    // generate return data
+    let returnData = [{
+        flag: 'https://www.countryflags.io/gb/flat/64.png',
+        symbol: '&pound;',
+        rate: '1.00'
+    }, {
+        flag: 'https://www.countryflags.io/eu/flat/64.png',
+        symbol: '&euro;',
+        rate: ratedata.EUR ? ratedata.EUR.toFixed(2) : "N/A"
+    }, {
+        flag: 'https://www.countryflags.io/us/flat/64.png',
+        symbol: '$',
+        rate: ratedata.USD ? ratedata.USD.toFixed(2) : "N/A"
+    }]
+
+    return returnData;
+};
