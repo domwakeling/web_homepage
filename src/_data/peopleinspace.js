521a8395f7843ef43ee0bd39e6ece0281b193154
Dom Wakeling <domwakeling@gmail.com>
2021-04-04 17:02:15 +0100

    Add "people in space" panel

---
+++ src/_data/peopleinspace.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+
+module.exports = async function () {
+
+    let spacedata = await axios
+        .get('http://api.open-notify.org/astros.json')
+        .catch((err) => {
+            console.error(err);
+            return []
+        });
+    
+    const mappedData = spacedata.data.people.reduce((cum, item) => {
+        if (item.craft in cum) {
+            cum[item.craft].push(item.name)
+        } else {
+            cum[item.craft] = [item.name]
+        }
+        return cum
+    }, {});
+
+    const outputData = [];
+    for (craft of Object.keys(mappedData)) {
+        console.log(craft)
+        outputData.push({
+            craft,
+            people: mappedData[craft].join(", "),
+            peopleCount: mappedData[craft].length
+        })
+    }
+
+    return outputData;
+};
