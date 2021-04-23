2f91ff32cdafec1600fb64b4da094a0265ce1b7d
Dom Wakeling <domwakeling@gmail.com>
2021-04-23 17:23:11 +0100

    Alternate flag api

--- src/_data/currency.js
+++ src/_data/currency.js
@@ -5,19 +5,31 @@ module.exports = async function () {
 
     // in development, send back a static object
     if (process.env.LOCAL_DEVELOPMENT == 'DEVELOPMENT') return [{
-        flag: 'https://www.countryflags.io/gb/flat/64.png',
+        // flag: 'https://www.countryflags.io/gb/flat/64.png',
+        flag: {
+            src: "https://flagcdn.com/64x48/gb.png",
+            srcset: "https://flagcdn.com/128x96/gb.png 2x, https://flagcdn.com/192x144/gb.png 3x"
+        },
         alt: 'UK flag',
         symbol: '&pound;',
         rate: '1.00'
     },
     {
-        flag: 'https://www.countryflags.io/eu/flat/64.png',
+        // flag: 'https://www.countryflags.io/eu/flat/64.png',
+        flag: {
+            src: "https://flagcdn.com/64x48/eu.png",
+            srcset: "https://flagcdn.com/128x96/eu.png 2x, https://flagcdn.com/192x144/eu.png 3x"
+        },
         symbol: '&euro;',
         alt: 'European flag',
         rate: '1.12'
     },
     {
-        flag: 'https://www.countryflags.io/us/flat/64.png',
+        // flag: 'https://www.countryflags.io/us/flat/64.png',
+        flag: {
+            src: "https://flagcdn.com/64x48/us.png",
+            srcset: "https://flagcdn.com/128x96/us.png 2x, https://flagcdn.com/192x144/us.png 3x"
+        },
         alt: 'US flag',
         symbol: '$',
         rate: '1.36'
@@ -36,17 +48,29 @@ module.exports = async function () {
 
     // generate return data
     let returnData = [{
-        flag: 'https://www.countryflags.io/gb/flat/64.png',
+        // flag: 'https://www.countryflags.io/gb/flat/64.png',
+        flag: {
+            src: "https://flagcdn.com/64x48/gb.png",
+            srcset: "https://flagcdn.com/128x96/gb.png 2x, https://flagcdn.com/192x144/gb.png 3x"
+        },
         alt: 'UK flag',
         symbol: '&pound;',
         rate: '1.00'
     }, {
-        flag: 'https://www.countryflags.io/eu/flat/64.png',
+        // flag: 'https://www.countryflags.io/eu/flat/64.png',
+        flag: {
+            src: "https://flagcdn.com/64x48/eu.png",
+            srcset: "https://flagcdn.com/128x96/eu.png 2x, https://flagcdn.com/192x144/eu.png 3x"
+        },
         alt: 'European flag',
         symbol: '&euro;',
         rate: ratedata.EUR && ratedata.GBP ? (ratedata.EUR / ratedata.GBP).toFixed(2) : "N/A"
     }, {
-        flag: 'https://www.countryflags.io/us/flat/64.png',
+        // flag: 'https://www.countryflags.io/us/flat/64.png',
+        flag: {
+            src: "https://flagcdn.com/64x48/us.png",
+            srcset: "https://flagcdn.com/128x96/us.png 2x, https://flagcdn.com/192x144/us.png 3x"
+        },
         alt: 'US flag',
         symbol: '$',
         rate: ratedata.USD && ratedata.GBP ? (ratedata.USD / ratedata.GBP).toFixed(2) : "N/A"
