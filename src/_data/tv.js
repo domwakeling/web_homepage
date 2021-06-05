dfd0fdc3963e3605989c357acebc31b819237a51
Dom Wakeling <domwakeling@gmail.com>
2021-06-05 11:31:06 +0100

    Add TV shows panel

---
+++ src/_data/tv.js
@@ -0,0 +1,39 @@
+const data = [
+    {
+        "showName": "Bosch",
+        "release": "25.06.21"
+    },
+    {
+        "showName": "Loki",
+        "release": "09.06.21"
+    },
+    {
+        "showName": "The Book Of Boba Fett",
+        "release": "25.12.21"
+    }
+];
+
+const today = new Date();
+
+const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+
+const diffDays = (d1, d2) => Math.round((d1 - d2) / oneDay);
+
+const daysUntil = (dStr) => {
+    const nums = dStr.split(".").map(n => parseInt(n))
+    const tDate = new Date(2000 + nums[2], nums[1] - 1, nums[0])
+    return diffDays(tDate, today)
+}
+
+module.exports = () => {
+    
+    const ret = data
+        .map(item => ({
+            "showName": item.showName,
+            "days": daysUntil(item.release)
+        }))
+        .filter(item => item.days >= 0)
+        .sort((i1, i2) => i1.days - i2.days);
+
+    return ret;
+}
