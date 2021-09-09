fbbb9aea25b48f86bc8948f5a426adfa38041234
Dom Wakeling <domwakeling@gmail.com>
2021-09-09 08:43:27 +0100

    Add fallback for joke data (seeing server errors)

--- src/_data/joke.js
+++ src/_data/joke.js
@@ -3,13 +3,15 @@ require('dotenv').config();
 
 module.exports = async function () {
 
-    // in development, send back a static object
-    if (process.env.LOCAL_DEVELOPMENT == 'DEVELOPMENT') return {
+    const dummydata = {
         "id": 26,
         "type": "programming",
         "setup": "If you put a million monkeys at a million keyboards, one of them will eventually write a Java program",
         "punchline": "the rest of them will write Perl"
     }
+
+    // in development, send back a static object
+    if (process.env.LOCAL_DEVELOPMENT == 'DEVELOPMENT') return dummydata;
 
     let jokedata = await axios
         .get('https://official-joke-api.appspot.com/jokes/programming/random')
@@ -18,5 +20,9 @@ module.exports = async function () {
             return {}
         });
 
-    return jokedata.data[0];
+    if (jokedata.data) {
+        return jokedata.data[0];
+    } else {
+        return dummydata;
+    }
 };
