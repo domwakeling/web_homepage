9c097b873fee564f7736913a0655d29dd217f78a
Dom Wakeling <domwakeling@gmail.com>
2022-04-17 13:30:09 +0100

    Replace "programmer joke" API
    - Deno version's filter endpoint appears broken
    - Also styling of attributions for 'space'

--- src/_data/joke.js
+++ src/_data/joke.js
@@ -7,21 +7,21 @@ module.exports = async function () {
         "id": 26,
         "type": "programming",
         "setup": "If you put a million monkeys at a million keyboards, one of them will eventually write a Java program",
-        "punchline": "the rest of them will write Perl"
+        "delivery": "the rest of them will write Perl"
     }
 
     // in development, send back a static object
     if (process.env.LOCAL_DEVELOPMENT == 'DEVELOPMENT') return dummydata;
 
     let jokedata = await axios
-        .get('https://joke.deno.dev/type/programming')
+        .get('https://v2.jokeapi.dev/joke/Programming')
         .catch((err) => {
             console.error(err);
             return {}
         });
 
     if (jokedata.data) {
-        return jokedata.data[Math.floor(Math.random() * jokedata.data.length)];
+        return jokedata.data;
     } else {
         return dummydata;
     }
