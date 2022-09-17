f27c47e2f1fbfc1e632118ca8568d0d6e2439429
Dom Wakeling <domwakeling@gmail.com>
2022-09-17 08:52:20 +0100

    deal with 14.0 lbs rounding

--- src/_includes/js/weight.js
+++ src/_includes/js/weight.js
@@ -8,6 +8,6 @@ const metToImp = () => {
     console.log("Kilos:", kilos);
     const pounds = kilos * CONV;
     const stone = Math.floor(pounds / 14);
-    const lbs = (pounds - 14 * stone).toFixed(1);
+    const lbs = (Math.floor((pounds - 14 * stone) * 10) * 0.1).toFixed(1);
     impBox.value = `${stone}st ${lbs}lbs`;
 }
