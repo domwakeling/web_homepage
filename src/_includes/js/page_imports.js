7ee7e9e9e119d40cd2d461427c118a66059b6c90
Dom Wakeling <domwakeling@gmail.com>
2023-07-13 22:22:10 +0100

    Minor a11y fixes

--- src/_includes/js/page_imports.js
+++ src/_includes/js/page_imports.js
@@ -1,7 +1,8 @@
 // add the masonry package
 const s = document.createElement('script');
 s.src = "https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js";
-document.body.appendChild(s);
+const m = document.getElementsByTagName('main')[0];
+m.appendChild(s);
 
 // remove media attribute so that font is applied (solution to non-render blocking on google font)
 const l = Array.from(document.getElementsByTagName('link'))
