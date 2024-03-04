5649611f6aea205bb27ea8251e7d04d2090a21aa
Dom Wakeling <domwakeling@gmail.com>
2024-03-04 23:03:45 +0000

    Remove Bebas Neue font

--- src/_data/archer.js
+++ src/_data/archer.js
@@ -75,7 +75,8 @@ const data = [
 ];
 
 module.exports = () => {
-    const ret = data[Math.floor(Math.random() * data.length)];
+    // const ret = data[Math.floor(Math.random() * data.length)];
+    const ret = data[5];
     const keys = Object.keys(ret);
     if (keys.indexOf("heightClass") < 0) {
         ret.heightClass = keys.indexOf('full_text') >= 0 ? "height3" : "height2";
